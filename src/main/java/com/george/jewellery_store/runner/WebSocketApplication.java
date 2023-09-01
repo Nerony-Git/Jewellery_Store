ab35b531bbc034b1671a389558c314070d62610d
George Amuzu <gnamuzu@gmail.com>
2023-09-01 23:24:24 +0100

    web socket application to run graphwalker test.

---
+++ src/main/java/com/george/jewellery_store/runner/WebSocketApplication.java
@@ -0,0 +1,26 @@
+package com.george.jewellery_store.runner;
+
+import com.george.jewellery_store.modelImpl.JewelleryAppTest;
+import org.graphwalker.java.test.Executor;
+import org.graphwalker.java.test.Result;
+import org.graphwalker.java.test.TestExecutor;
+import org.graphwalker.websocket.WebSocketServer;
+
+import java.io.IOException;
+
+public class WebSocketApplication {
+    public static void main(String[] args) throws IOException {
+        Executor executor = new TestExecutor(JewelleryAppTest.class);
+
+        WebSocketServer server = new WebSocketServer(8887, executor.getMachine());
+        server.start();
+
+        Result result = executor.execute(true);
+        if (result.hasErrors()) {
+            for (String error : result.getErrors()) {
+                System.out.println(error);
+            }
+        }
+        System.out.println("Done: [" + result.getResults().toString(2) + "]");
+    }
+}
