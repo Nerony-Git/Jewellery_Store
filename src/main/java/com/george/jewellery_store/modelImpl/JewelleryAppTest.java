5a3dc7b24935220a005856e31a735ef78bf473ac
George Amuzu <gnamuzu@gmail.com>
2023-09-02 00:18:38 +0100

    added Jewellery store GraphWalker test.

---
+++ src/main/java/com/george/jewellery_store/modelImpl/JewelleryAppTest.java
@@ -0,0 +1,268 @@
+package com.george.jewellery_store.modelImpl;
+
+import com.george.jewellery_store.Jewellery;
+import org.graphwalker.core.machine.ExecutionContext;
+import org.graphwalker.java.annotation.GraphWalker;
+
+@GraphWalker(value = "random(edge_coverage(100))", start = "e_init")
+public class JewelleryAppTest extends ExecutionContext implements Jewellery {
+    @Override
+    public void e_CheckoutFailed() {
+
+    }
+
+    @Override
+    public void v_Shop() {
+
+    }
+
+    @Override
+    public void e_AboutUs() {
+
+    }
+
+    @Override
+    public void e_Wishlist() {
+
+    }
+
+    @Override
+    public void e_ResgisterSuccess() {
+
+    }
+
+    @Override
+    public void e_Logout() {
+
+    }
+
+    @Override
+    public void e_ContactUs() {
+
+    }
+
+    @Override
+    public void e_Shop() {
+
+    }
+
+    @Override
+    public void v_AddToCart() {
+
+    }
+
+    @Override
+    public void v_Orders() {
+
+    }
+
+    @Override
+    public void e_ViewEditAccountSuccess() {
+
+    }
+
+    @Override
+    public void e_AddToCart() {
+
+    }
+
+    @Override
+    public void v_ProductDetails() {
+
+    }
+
+    @Override
+    public void e_Wishlist_Account() {
+
+    }
+
+    @Override
+    public void e_ProductDetails() {
+
+    }
+
+    @Override
+    public void e_Orders() {
+
+    }
+
+    @Override
+    public void e_ViewCart_Account() {
+
+    }
+
+    @Override
+    public void v_Homepage() {
+
+    }
+
+    @Override
+    public void e_LoginFailed() {
+
+    }
+
+    @Override
+    public void e_AddToWishlist() {
+
+    }
+
+    @Override
+    public void e_Account() {
+
+    }
+
+    @Override
+    public void v_ContactUs() {
+
+    }
+
+    @Override
+    public void v_ViewEditAccount() {
+
+    }
+
+    @Override
+    public void e_BackToProductDetails() {
+
+    }
+
+    @Override
+    public void e_ViewEditAccountFailede() {
+
+    }
+
+    @Override
+    public void v_Checkout() {
+
+    }
+
+    @Override
+    public void e_Login() {
+
+    }
+
+    @Override
+    public void v_Resgister() {
+
+    }
+
+    @Override
+    public void e_Resgister() {
+
+    }
+
+    @Override
+    public void v_AboutUs() {
+
+    }
+
+    @Override
+    public void e_Description() {
+
+    }
+
+    @Override
+    public void e_LoginSuccess() {
+
+    }
+
+    @Override
+    public void v_Info() {
+
+    }
+
+    @Override
+    public void v_Reviews() {
+
+    }
+
+    @Override
+    public void v_Wishlist() {
+
+    }
+
+    @Override
+    public void e_Checkout() {
+
+    }
+
+    @Override
+    public void e_ResgisterFailed() {
+
+    }
+
+    @Override
+    public void v_Login() {
+
+    }
+
+    @Override
+    public void v_Cart() {
+
+    }
+
+    @Override
+    public void e_Info() {
+
+    }
+
+    @Override
+    public void e_CheckoutSuccess() {
+
+    }
+
+    @Override
+    public void v_Account() {
+
+    }
+
+    @Override
+    public void v_Description() {
+
+    }
+
+    @Override
+    public void e_Homepage() {
+
+    }
+
+    @Override
+    public void e_ViewCart() {
+
+    }
+
+    @Override
+    public void v_AddToWishlist() {
+
+    }
+
+    @Override
+    public void e_Exit() {
+
+    }
+
+    @Override
+    public void e_Cart() {
+
+    }
+
+    @Override
+    public void e_init() {
+
+    }
+
+    @Override
+    public void e_Reviews() {
+
+    }
+
+    @Override
+    public void v_ViewCart() {
+
+    }
+
+    @Override
+    public void e_ViewEditAccount() {
+
+    }
+}
