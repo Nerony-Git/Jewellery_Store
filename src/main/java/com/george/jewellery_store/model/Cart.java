669512307cc8644781d4a19712e0683f4d6bf6f4
George Amuzu <gnamuzu@gmail.com>
2023-09-01 23:55:15 +0100

    added cart model.

---
+++ src/main/java/com/george/jewellery_store/model/Cart.java
@@ -0,0 +1,80 @@
+package com.george.jewellery_store.model;
+
+public class Cart {
+    private String productID;
+    private String customerID;
+    private String sessionID;
+    private int quantity;
+    private String productName;
+    private String productImagePath;
+    private Float productPrice;
+
+    public Cart(){
+
+    }
+
+    public Cart(String productID, String productName, String productImagePath, Float productPrice, int quantity) {
+        super();
+        this.productID = productID;
+        this.productName = productName;
+        this.productImagePath = productImagePath;
+        this.productPrice = productPrice;
+        this.quantity = quantity;
+    }
+
+    public String getProductID() {
+        return productID;
+    }
+
+    public void setProductID(String productID) {
+        this.productID = productID;
+    }
+
+    public String getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(String customerID) {
+        this.customerID = customerID;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getSessionID() {
+        return sessionID;
+    }
+
+    public void setSessionID(String sessionID) {
+        this.sessionID = sessionID;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductImagePath() {
+        return productImagePath;
+    }
+
+    public void setProductImagePath(String productImagePath) {
+        this.productImagePath = productImagePath;
+    }
+
+    public Float getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(Float productPrice) {
+        this.productPrice = productPrice;
+    }
+}
