13bd4c457241cacd465290701d5cbf079d8b1756
George Amuzu <gnamuzu@gmail.com>
2023-09-02 00:00:39 +0100

    added servlet.

--- src/main/java/com/george/jewellery_store/HelloServlet.java
+++ src/main/java/com/george/jewellery_store/HelloServlet.java
@@ -1,26 +1,42 @@
 package com.george.jewellery_store;
 
 import java.io.*;
+import java.sql.SQLException;
+import java.util.List;
 
+import com.george.jewellery_store.dao.ProductDao;
+import com.george.jewellery_store.model.Product;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
+@WebServlet("")
 public class HelloServlet extends HttpServlet {
-    private String message;
+    private static final long serialVersionUID = 1L;
+    private ProductDao productDao = new ProductDao();
 
     public void init() {
-        message = "Hello World!";
+
     }
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // Assuming productDao is your DAO for fetching products
+        List<Product> allProducts = null;
+        try {
+            allProducts = productDao.getAllProducts();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        request.setAttribute("allProducts", allProducts);
 
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
+        HttpSession session = request.getSession();
+        String sessionID = session.getId();
+        session.setAttribute("sessionID", sessionID);
+
+        // Forward the request to the JSP page
+        getServletContext().getRequestDispatcher("/index.jsp").forward(request, response);
     }
 
     public void destroy() {
